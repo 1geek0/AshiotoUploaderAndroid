6e363bc381b70ef2717e61393c214e72820ca23f
NilayKulkarni <typhoscientic@gmail.com>
2016-11-18 20:19:48 +0530

    Ported to bluetooth list activity. Will also add settings gateway soon

--- app/src/main/java/xyz/ashioto/ashioto/HomeActivity.java
+++ app/src/main/java/xyz/ashioto/ashioto/HomeActivity.java
@@ -1,12 +1,22 @@
 package xyz.ashioto.ashioto;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import butterknife.BindView;
@@ -14,17 +24,26 @@ import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @BindView(R.id.tablayout_main) //TabLayout
-            TabLayout tabLayout_main;
-    @BindView(R.id.viewpager) // Viewpager (handles tabs)
-            ViewPager viewPager;
     @BindView(R.id.start_toolbar) //Toolbar
-            Toolbar start_toolbar;
+    Toolbar start_toolbar;
     @BindView(R.id.eventsSpinner)//Spinner holding events list
-            Spinner eventsSpinner;
+    Spinner eventsSpinner;
 
     //Bluetooth variable declaration
     BluetoothSPP bluetoothSPP = new BluetoothSPP(HomeActivity.this);
+
+
+    @BindView(R.id.bluetooth_list) //RecyclerView for bluetooth list
+    RecyclerView bluetooth_list;
+
+    RecyclerView.Adapter bluetooth_list_adapter; //Adapter for bluetooth list
+
+    //Arraylist for HC devices
+    ArrayList<String> HCDeviceNameList = new ArrayList<>();
+    ArrayList<String> HCDeviceAddressList = new ArrayList<>();
+
+    //Set containing bonded devices
+    Set<BluetoothDevice> BondedDeviceSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +55,45 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
-    }
-
-    private void setUpBluetooth() {
-        if (!bluetoothSPP.isBluetoothEnabled()) { //Check if bluetooth is enabled
-            bluetoothSPP.enable(); //Enable bluetooth if not enabled already
-        }
+        setUpBluetoothList();
+        setUpBluetoothAdapters();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         bluetoothSPP.getBluetoothAdapter().disable(); //Disable bluetooth once the app is closed
+    }
+
+
+    private void setUpBluetoothList(){ //Populates device list
+        BondedDeviceSet = bluetoothSPP.getBluetoothAdapter().getBondedDevices();
+        if (BondedDeviceSet.size() > 0){
+            for (BluetoothDevice currentDevice: BondedDeviceSet){
+                if (currentDevice.getName().startsWith("HC")){
+                    HCDeviceNameList.add(currentDevice.getName());//Add device name to list for RecyclerView
+                    HCDeviceAddressList.add(currentDevice.getAddress()); //Add device address for connection
+                }
+            }
+        }
+        if (bluetoothSPP.isDiscovery()){
+            bluetoothSPP.cancelDiscovery();
+        }
+        bluetoothSPP.startDiscovery(); //Start device discovery
+    }
+
+    private void setUpBluetoothAdapters(){ // Sets Listeners and adapters
+        bluetooth_list.setLayoutManager(new LinearLayoutManager(HomeActivity.this));
+        bluetooth_list.addOnItemTouchListener(new RecyclerItemClickListener(HomeActivity.this, new RecyclerItemClickListener.SimpleOnItemClickListener() {
+            @Override
+            public void onItemClick(AppCompatTextView childView, int position) {
+                super.onItemClick(childView, position);
+                Intent bluetoothConnectIntent = new Intent(HomeActivity.this, BluetoothActivity.class);
+                bluetoothConnectIntent.putExtra("device-address", HCDeviceAddressList.get(position));
+                startActivity(bluetoothConnectIntent);
+            }
+        }));
+        bluetooth_list_adapter = new RecyclerAdapter_bluetooth(HCDeviceNameList); //Feed device names to RecyclerView
+        bluetooth_list.setAdapter(bluetooth_list_adapter);
     }
 }
