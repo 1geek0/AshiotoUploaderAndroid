e5ae48f9c3d5d6587676395ebb5e04c33a94f390
NilayKulkarni <typhoscientic@gmail.com>
2016-11-21 20:06:46 +0530

    Added Butterknife binding

--- app/src/main/java/xyz/ashioto/ashioto/BluetoothActivity.java
+++ app/src/main/java/xyz/ashioto/ashioto/BluetoothActivity.java
@@ -1,13 +1,15 @@
 package xyz.ashioto.ashioto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 public class BluetoothActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+        ButterKnife.bind(this);
     }
 }
