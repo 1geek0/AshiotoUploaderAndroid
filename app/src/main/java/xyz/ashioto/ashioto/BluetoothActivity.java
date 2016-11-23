c897d45364bb9b7f490f0dd3b81b4816bfb13bba
NilayKulkarni <typhoscientic@gmail.com>
2016-11-23 17:40:34 +0530

    Now the device is disconnects when onPause. Added data listener and the data is set to the TextView. Fixed ProgressDialog bug.

--- app/src/main/java/xyz/ashioto/ashioto/BluetoothActivity.java
+++ app/src/main/java/xyz/ashioto/ashioto/BluetoothActivity.java
@@ -3,10 +3,12 @@ package xyz.ashioto.ashioto;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
 import android.widget.Toast;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import app.akexorcist.bluetotohspp.library.BluetoothState;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BluetoothActivity extends AppCompatActivity {
@@ -14,7 +16,10 @@ public class BluetoothActivity extends AppCompatActivity {
     //BluetoothSPP setup
     BluetoothSPP spp = new BluetoothSPP(BluetoothActivity.this);
     //ProgressDialog
-    ProgressDialog progressDialog = new ProgressDialog(BluetoothActivity.this);
+    ProgressDialog progressDialog;
+    //TextView for count
+    @BindView(R.id.mainCount)
+    AppCompatTextView countTextView;
     //Connection listener to handle bluetooth related tasks
     BluetoothSPP.BluetoothConnectionListener bluetoothConnectionListener = new BluetoothSPP.BluetoothConnectionListener() {
         @Override
@@ -35,8 +40,16 @@ public class BluetoothActivity extends AppCompatActivity {
         @Override
         public void onDeviceConnectionFailed() {
             //If connection fails, finish activity and give a toast to the user
-            t("Conncetion Failed");
+            t("Connection Failed");
             finish();
+        }
+    };
+    //DataReceive listener
+    BluetoothSPP.OnDataReceivedListener dataReceivedListener = new BluetoothSPP.OnDataReceivedListener() {
+        @Override
+        public void onDataReceived(byte[] data, String message) {
+            String count = message.substring(message.lastIndexOf('#') + 1, message.lastIndexOf('$')); //Get the subtring between # and $ (logic from old code)
+            countTextView.setText(count);
         }
     };
     //Device mac address
@@ -55,20 +68,33 @@ public class BluetoothActivity extends AppCompatActivity {
         spp.startService(BluetoothState.DEVICE_OTHER);
 
         //ProgressDialog setup
+        progressDialog = new ProgressDialog(BluetoothActivity.this); //Assignment done here because doing it with declaration would crash the app
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.setIndeterminate(true);
+        // TODO: 23/11/16 The spinner is not showing. Fix it.
         progressDialog.setTitle("Connecting To Ashioto");
+        progressDialog.show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        //Set connection listener for bluetooth spp
         spp.setBluetoothConnectionListener(bluetoothConnectionListener);
+        //Set data receiver for bluetooth spp
+        spp.setOnDataReceivedListener(dataReceivedListener);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         spp.connect(mDeviceAddress);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        spp.disconnect();
     }
 
     //Easier toast
